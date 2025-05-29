f45c0331ea3ab2b14d492695f0651ea7c6e1c8eb
LevchenkoOlexsii <realcarter50@gmail.com>
2025-05-29 21:39:35 +0300

    Initial Commit

---
+++ TwinTalk/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TwinTalk
+//
+//  Created by Olexsii Levchenko on 29.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TwinTalk/TwinTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwinTalkApp.swift
+//  TwinTalk
+//
+//  Created by Olexsii Levchenko on 29.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct TwinTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
